025daa0a1f2dd32059997ed2685b433f9f408313
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-03-18 14:17:34 -0700

    Create assignment module in core package

---
+++ gradecalc/core/assignment.py
@@ -0,0 +1,28 @@
+from attr import attrs, attrib
+from attr.converters import optional
+
+
+@attrs(frozen=True, eq=True)
+class Assignment:
+    score = attrib(converter=float)
+    total = attrib(converter=float)
+    name = attrib(default=None, converter=optional(str))
+
+    @property
+    def grade(self):
+        return self.score / self.total
+
+    @total.validator
+    def check_total(self, attribute, value):
+        if value <= 0:
+            raise ValueError("Total must be greater than zero")
+
+    def __add__(self, other):
+        return Assignment(self.score + other.score, self.total + other.total, None)
+
+    def __str__(self):
+        return f"{self.percentage:.4f}"
+
+    @classmethod
+    def fromdict(cls, data):
+        return cls(data["recieved"], data["possible"], data.get("name", None))
