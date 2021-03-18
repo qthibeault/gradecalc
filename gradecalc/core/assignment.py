61e45440e65ef859b7091e0b5f948d4af1634daa
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-03-18 14:35:17 -0700

    Revert property name to percentage

--- gradecalc/core/assignment.py
+++ gradecalc/core/assignment.py
@@ -9,19 +9,19 @@ class Assignment:
     name = attrib(default=None, converter=optional(str))
 
     @property
-    def grade(self):
+    def percentage(self):
         return self.score / self.total
-
-    @total.validator
-    def check_total(self, attribute, value):
-        if value <= 0:
-            raise ValueError("Total must be greater than zero")
 
     def __add__(self, other):
         return Assignment(self.score + other.score, self.total + other.total, None)
 
     def __str__(self):
         return f"{self.percentage:.4f}"
+
+    @total.validator
+    def check_total(self, attribute, value):
+        if value <= 0:
+            raise ValueError("Total must be greater than zero")
 
     @classmethod
     def fromdict(cls, data):
